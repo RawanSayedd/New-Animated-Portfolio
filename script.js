ef56bd40ec4b03e14b4a6a6211c3db4f8cae1bf7
RawanSayedd <rawansayed143@gmail.com>
2023-09-12 00:21:56 +0300

    modified

--- script.js
+++ script.js
@@ -42,13 +42,64 @@ $(".carousel .carousel-item").each(function () {
     next.children(":first-child").clone().appendTo($(this));
   }
 });
-// (function () {
-//   "use strict";
+//scroll animation
+// const boxes = document.querySelectorAll(".content");
+// function checkBoxes() {
+//   const triggerBottom = (window.innerHeight / 5) * 4;
+//   boxes.forEach((box, idx) => {
+//     const boxTop = box.getBoundingClientRect().top;
+//     if (boxTop < triggerBottom) {
+//       box.classList.add("show");
+//     } else {
+//       box.classList.add("show");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", checkBoxes);
 
-//   let preloader = select("#preloader");
-//   if (preloader) {
-//     window.addEventListener("load", () => {
-//       preloader.remove();
-//     });
-//   }
-// })();
+//progres bar animated
+function move() {
+  var skills = [
+    { id: "myBar1", targetWidth: 95 },
+    { id: "myBar2", targetWidth: 85 },
+    { id: "myBar3", targetWidth: 80 },
+    { id: "myBar4", targetWidth: 85 },
+    { id: "myBar5", targetWidth: 70 },
+  ];
+
+  for (var i = 0; i < skills.length; i++) {
+    var skill = skills[i];
+    if (!skill.animated && isScrolledIntoView(skill.id)) {
+      animateProgress(skill.id, skill.targetWidth);
+      skill.animated = true;
+    }
+  }
+}
+function animateProgress(id, targetWidth) {
+  var elem = document.getElementById(id);
+  var width = 0;
+  var increment = 1;
+  var id = setInterval(frame, 7);
+
+  function frame() {
+    if (width >= targetWidth) {
+      clearInterval(id);
+    } else {
+      width += increment;
+      elem.style.width = width + "%";
+    }
+  }
+}
+
+function isScrolledIntoView(id) {
+  var elem = document.getElementById(id);
+  var rect = elem.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  // Check if the element is fully visible in the viewport
+  var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+  return isVisible;
+}
+
+window.addEventListener("scroll", move);
